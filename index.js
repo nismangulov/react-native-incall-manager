65d2814e786ea710a3e8c938efd5b0f4fa39f57c
Nail Ismangulov <n.ismangulov@gmail.com>
2017-05-15 17:58:00 +0600

    Added ability to start and stop proximity sensor from RN

--- index.js
+++ index.js
@@ -45,16 +45,13 @@ class InCallManager {
         _InCallManager.turnScreenOn();
     }
 
-    setFlashOn(enable, brightness) {
-        if (Platform.OS === 'ios') {
-            enable = (enable === true) ? true : false;
-            brightness = (typeof brightness === 'number') ? brightness : 0;
-            _InCallManager.setFlashOn(enable, brightness);
-        } else {
-            console.log("Android doesn't support setFlashOn(enable, brightness)");
-        }
+    startProximitySensor() {
+        _InCallManager.startProximitySensor();
     }
 
+    stopProximitySensor() {
+        _InCallManager.stopProximitySensor();
+    }
 
     setKeepScreenOn(enable) {
         enable = (enable === true) ? true : false;
